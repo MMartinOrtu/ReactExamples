48ace0b7ad8d7438575dc55877341b1861d2bb59
Moyayi <pedro.moyano.garcia@gmail.com>
2021-01-28 11:27:53 +0100

    React life cicle example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Child from './components/Child'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  console.log('%cApp: render start', 'color: tomato');
+
+  const [showChild, setShowChild] = React.useState(() => {
+    console.log('%cApp: useState callback', 'color: tomato');
+    return false;
+  });
+
+  React.useEffect(() => {
+    console.log('%cApp: useEffect no deps', 'color: LightCoral')
+    return () => {
+      console.log('%cApp: useEffect no deps cleanup', 'color: LightCoral')
+    }
+  })
+  
+  React.useEffect(() => {
+    console.log('%cApp: useEffect empty deps', 'color: MediumTurquoise')
+    return () => {
+      console.log('%cApp: useEffect empty cleanup', 'color: MediumTurquoise');
+    }
+  }, [])
+
+  React.useEffect(() => {
+    console.log('%cApp: useEffect with dep', 'color: HotPink')
+    return () => {
+      console.log('%cApp: useEffect with dep cleanup', 'color: HotPink')
+    }
+  }, [showChild])
+
+  const element = (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={showChild}
+          onChange={evt => setShowChild(evt.target.checked)}
+        />{' '}
+        show child
+        
+      </label>
+      <div className="box">
+        {showChild ? <Child/> : null}
+      </div>
+    </>
+  ) 
+  return element;
 }
 
 export default App;
---
+++ src/components/Child.js
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from 'react'
+
+export default function Child(){
+    console.log(
+        '%c         Child: render start', 
+        'color: MediumSpringGreen')
+
+    const [count, setCount] = useState(() => {
+        console.log(
+            '%c         Child: useState callback', 
+            'color: tomato')
+
+        return 0
+    })
+
+    useEffect(() => {
+        console.log(
+            '%c         Child: useEffect no deps', 
+            'color: LightCoral')
+
+        return () => {
+            console.log(
+                '%c         Child: useEffect no deps cleanup', 
+                'color: LightCoral')
+        }
+    })
+    
+    useEffect(() => {
+        console.log(
+            '%c         Child: useEffect empty deps', 
+            'color: MediumTurquoise')
+        return () =>{
+            console.log(
+                '%c         Child: useEffect empty deps cleanup', 
+                'color: MediumTurquoise')
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(
+            '%c         Child: useEffect empty deps', 
+            'color: HotPink')
+        return () =>{
+            console.log(
+                '%c         Child: useEffect empty deps cleanup', 
+                'color: HotPink')
+        }
+    }, [count])
+
+    const element = (
+        <button
+            onClick={() => setCount(previousCount => previousCount + 1)}
+        >
+            {count}
+        </button>
+    )
+    
+    console.log('%cChild: render end', 'color: MediumSpringGreen')
+    return element
+}
+
